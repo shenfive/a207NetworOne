42a07b6d179549f0c20873386d96edf78e935d46
Danny Shen <shenfive@gmail.com>
2020-08-15 16:39:22 +0800

    brewerser

--- a207NetworOne/ViewController.swift
+++ a207NetworOne/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var status: UILabel!
     @IBOutlet weak var theWeb: WKWebView!
     
+    @IBOutlet weak var urlAddTF: UITextField!
     var timer:Timer? = nil
     
     override func viewDidLoad() {
@@ -22,15 +23,32 @@ class ViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(getNetworkStatus(sender:)), userInfo: nil, repeats: true)
         
         
-        if let url = URL(string: "https://www.apple.com.tw/"){
-            let request = URLRequest(url: url)
-            theWeb.load(request)
-        }
+        openURL(urlString: "www.apple.com")
         
         
         
     }
 
+    func openURL(urlString:String){
+        
+        let target = "https://" + urlString
+        
+        if let url = URL(string: target){
+            let request = URLRequest(url: url)
+            theWeb.load(request)
+        }
+    }
+    
+    
+    @IBAction func openWeb(_ sender: Any) {
+        if let url = urlAddTF.text{
+            openURL(urlString: url)
+        }
+    }
+    
+    
+    
+    
     @objc func getNetworkStatus(sender:Any?)->Bool{
         if Reachability(hostName: "www.apple.com")?.currentReachabilityStatus().rawValue == 0 {
             status.text = "沒網路"
